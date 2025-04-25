b891b31bbd78f4bbe3157350ca3826e258426b30
VedantVardhaan <vvardhaan@ucsd.edu>
2025-04-24 19:47:22 -0700

    lab4_1

--- global.js
+++ global.js
@@ -106,7 +106,42 @@ form?.addEventListener("submit", function (event) {
   location.href = url;
 });
 
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  for (const project of projects) {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  }
+}
+
+export async function fetchGitHubData() {
+  return fetchJSON(`https://api.github.com/users/VedVar43789`);
+}
 
 
-  
 
